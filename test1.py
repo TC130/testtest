75f807b15eb96e0ae7188199772bebc0d97c142e
zhaorui5 <zhaorui5@guazi.com>
2020-02-25 11:50:06 +0800

    2

--- test1.py
+++ test1.py
@@ -1,22 +1,3 @@
 import re
 print("test")
-print("test1447")
-print("test1454")
-print("test1639")
-print("test1713")
-print("test1727")
-print("test1509")
-print("test1509")
-print("test1509")
-print("test1509")
-print("test1509")
-print("test1509")
-print("test1509")
-print("test1509")
-print("test1509")
-
-
-print("test1509")
-print("test1509")
-
-
+print("1149")
